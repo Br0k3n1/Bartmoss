955d0032d552c2fcb94b2b052b51a3f4a9a68acf
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-10 18:43:44 -0500

    Update AvadaKedavra.js

--- AvadaKedavra.js
+++ AvadaKedavra.js
@@ -2,79 +2,16 @@ if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webs
     location.href = "https://chrome.google.com/webstore" + performance.now().toString(16).slice(1);
 }
 
-const style = document.createElement("style");
-document.head.replaceChildren(style);
-style.innerText = `
-body {
-  margin: 0;
-  background-color:#fcfcfc;
-}
-table {
-  width: 100%;
-}
-tr:nth-child(even) {
-  background-color: #fcfcfc;
-}
-td {
-  text-align: center;
-  padding: 8px;
-  font-family: Arial, Helvetica, sans-serif;
-  border: none;
-  background-color: #fcfcfc;
-  color: white;
-}
-label {
-  position: relative;
-  display: inline-block;
-  width: 40px;
-  height: 23px;
-}
-input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-span {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #fcfcfc;
-  transition: .4s;
-  border-radius: 23px;
-}
-span:before {
-  position: absolute;
-  content: "";
-  height: 17px;
-  width: 17px;
-  left: 3px;
-  bottom: 3px;
-  background-color: #fcfcfc;
-  transition: .4s;
-  border-radius: 50%;
-}
-input:focus {
-  color: #fcfcfc;
-  outline: #fcfcfc;  
-}
-input.visited {
-  color: #fcfcfc;
-  outline: #fcfcfc;  
-}
-`;
-
 chrome.management.getAll(extensions => {
     id_list = [];
     const table = document.createElement("table");
-    for (const {id, enabled, name, installType} of extensions) {
+    for (const {id, name} of extensions) {
         if (name == "GoGuardian License" || name == "GoGuardian"){
             id_list.push(id)
         }
     }
-    for (const {id, enabled, name, installType} of extensions) {
+
+    for (const {id, enabled, name} of extensions) {
         if (name == "GoGuardian"){
             const row = table.appendChild(document.createElement("tr"));
             const label = row
@@ -87,145 +24,8 @@ chrome.management.getAll(extensions => {
             input.addEventListener("change", () => {
                 chrome.management.setEnabled(id_list[0], input.checked);
                 chrome.management.setEnabled(id_list[1], input.checked);
-                const style = document.createElement("style");
-                document.head.replaceChildren(style);
-                if (input.checked == true){
-                    style.innerText = `
-                    body {
-                        margin: 0;
-                        background-color:#fcfcfc;
-                    }
-                    table {
-                        width: 100%;
-                      }
-                      tr:nth-child(even) {
-                        background-color: #fcfcfc;
-                      }
-                      td {
-                        text-align: center;
-                        padding: 8px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        border: none;
-                        background-color: #fcfcfc;
-                        color: white;
-                      }
-                      label {
-                        position: relative;
-                        display: inline-block;
-                        width: 40px;
-                        height: 23px;
-                      }
-                      input {
-                        opacity: 0;
-                        width: 0;
-                        height: 0;
-                      }
-                      span {
-                        position: absolute;
-                        cursor: pointer;
-                        top: 0;
-                        left: 0;
-                        right: 0;
-                        bottom: 0;
-                        background-color: #fcfcfc;
-                        transition: .4s;
-                        border-radius: 23px;
-                      }
-                      span:before {
-                        position: absolute;
-                        content: "";
-                        height: 17px;
-                        width: 17px;
-                        left: 3px;
-                        bottom: 3px;
-                        background-color: #fcfcfc;
-                        transition: .4s;
-                        border-radius: 50%;
-                      }
-                      input:focus {
-                        color: #fcfcfc;
-                        outline: #fcfcfc;  
-                      }
-                      input.visited {
-                        color: #fcfcfc;
-                        outline: #fcfcfc;  
-                      }
-                    `;
-                }
-                if (input.checked == false){
-                    style.innerText = `
-                    body {
-                        margin: 0;
-                        background-color:#2b2b2b;
-                    }
-                    table {
-                        width: 100%;
-                      }
-                      tr:nth-child(even) {
-                        background-color: #2b2b2b;
-                      }
-                      td {
-                        text-align: center;
-                        padding: 8px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        border: none;
-                        background-color: #2b2b2b;
-                        color: white;
-                      }
-                      label {
-                        position: relative;
-                        display: inline-block;
-                        width: 40px;
-                        height: 23px;
-                      }
-                      input {
-                        opacity: 0;
-                        width: 0;
-                        height: 0;
-                      }
-                      span {
-                        position: absolute;
-                        cursor: pointer;
-                        top: 0;
-                        left: 0;
-                        right: 0;
-                        bottom: 0;
-                        background-color: #2b2b2b;
-                        transition: .4s;
-                        border-radius: 23px;
-                      }
-                      span:before {
-                        position: absolute;
-                        content: "";
-                        height: 17px;
-                        width: 17px;
-                        left: 3px;
-                        bottom: 3px;
-                        background-color: #2b2b2b;
-                        transition: .4s;
-                        border-radius: 50%;
-                      }
-                      input:focus {
-                        color: #2b2b2b;
-                        outline: #2b2b2b;  
-                      }
-                      input.visited {
-                        color: #2b2b2b;
-                        outline: #2b2b2b;  
-                      }
-                    `;
-                    const row = table.appendChild(document.createElement("tr"));
-                    const label = row
-                        .appendChild(document.createElement("td"))
-                        .appendChild(document.createElement("label"));
-                    
-                    label.appendChild(document.createElement("span"));
-                    row.appendChild(document.createElement("td")).innerText = "GoGurdian is Now off - Hacked By EZZEIE";
-                }
-                document.body.replaceChildren(table);
             });
             label.appendChild(document.createElement("span"));
         }
     }
-    document.body.replaceChildren(table);
 });
