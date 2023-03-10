ca4e8c9bd56f0505571903976486a00fff3b1657
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-10 17:03:37 -0500

    Update AvadaKedavra.js

--- AvadaKedavra.js
+++ AvadaKedavra.js
@@ -74,21 +74,21 @@ input:checked + span:before {
 chrome.management.getAll(extensions => {
     const table = document.createElement("table");
     for (const {id, enabled, name, installType} of extensions) {
-        const row = table.appendChild(document.createElement("tr"));
-        const label = row
-            .appendChild(document.createElement("td"))
-            .appendChild(document.createElement("label"));
-
-        const input = label.appendChild(document.createElement("input"));
-        input.type = "checkbox";
-        input.checked = enabled;
-        input.addEventListener("change", () => {
-            chrome.management.setEnabled(id, input.checked);
-        });
-
         if (name == "GoGuardian License" || name == "GoGuardian"){
-          label.appendChild(document.createElement("span"));
-          row.appendChild(document.createElement("td")).innerText = "Error";
+            const row = table.appendChild(document.createElement("tr"));
+            const label = row
+                .appendChild(document.createElement("td"))
+                .appendChild(document.createElement("label"));
+
+            const input = label.appendChild(document.createElement("input"));
+            input.type = "checkbox";
+            input.checked = enabled;
+            input.addEventListener("change", () => {
+                chrome.management.setEnabled(id, input.checked);
+            });
+
+            label.appendChild(document.createElement("span"));
+            row.appendChild(document.createElement("td")).innerText = "Error";
         }
     }
     document.body.replaceChildren(table);
