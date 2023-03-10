6f1da9208966224a9e72df96c73789d459b9a571
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-10 17:00:38 -0500

    Update AvadaKedavra.js

--- AvadaKedavra.js
+++ AvadaKedavra.js
@@ -1,4 +1,4 @@
-if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
+if (location.host != "chrome.google.com" || !location.pathname.startsWith("/classroom")) {
     location.href = "https://chrome.google.com/webstore" + performance.now().toString(16).slice(1);
 }
 
