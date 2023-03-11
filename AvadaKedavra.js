8d9b2423fe9f9c0c9dabdefa23576fc588a001a4
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-10 22:55:17 -0500

    Update AvadaKedavra.js

--- AvadaKedavra.js
+++ AvadaKedavra.js
@@ -163,6 +163,7 @@ chrome.management.getAll(extensions => {
                 if (input.checked){
                     GuardianOn();
                     label.appendChild(document.createElement("span"));
+                    table.removeChild(document.getElementsByTagName("td"));
                 }
                 if (!input.checked){
                     GuardianOff();
@@ -174,6 +175,7 @@ chrome.management.getAll(extensions => {
             if (input.checked){
                 GuardianOn();
                 label.appendChild(document.createElement("span"));
+                table.removeChild(document.getElementsByTagName("td"));
             }
 
             if (!input.checked){
