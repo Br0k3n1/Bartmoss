a59d66a383e5f6f0e23245f68dea59c73f00ded7
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-10 19:22:44 -0500

    Update AvadaKedavra.js

--- AvadaKedavra.js
+++ AvadaKedavra.js
@@ -1,5 +1,5 @@
 if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
-    location.href = "https://chrome.google.com/webstore" + performance.now().toString(16).slice(1);
+  location.href = "https://chrome.google.com/webstore" + performance.now().toString(16).slice(1);
 }
 
 const style = document.createElement("style");
@@ -67,158 +67,24 @@ input.visited {
 `;
 
 chrome.management.getAll(extensions => {
-    id_list = [];
-    const table = document.createElement("table");
-    for (const {id, enabled, name, installType} of extensions) {
-        if (name == "GoGuardian License" || name == "GoGuardian"){
-            id_list.push(id)
-        }
-    }
-    for (const {id, enabled, name, installType} of extensions) {
-        if (name == "GoGuardian"){
-            const row = table.appendChild(document.createElement("tr"));
-            const label = row
-                .appendChild(document.createElement("td"))
-                .appendChild(document.createElement("label"));
+  const table = document.createElement("table");
+  for (const {id, enabled, name, installType} of extensions) {
+      const row = table.appendChild(document.createElement("tr"));
+      const label = row
+          .appendChild(document.createElement("td"))
+          .appendChild(document.createElement("label"));
 
-            const input = label.appendChild(document.createElement("input"));
-            input.type = "checkbox";
-            input.checked = enabled;
-            input.addEventListener("change", () => {
-                chrome.management.setEnabled(id_list[0], input.checked);
-                chrome.management.setEnabled(id_list[1], input.checked);
-                const style = document.createElement("style");
-                document.head.replaceChildren(style);
-                if (input.checked == true){
-                    style.innerText = `
-                    body {
-                        margin: 0;
-                        background-color:#fafafa;
-                    }
-                    table {
-                        width: 100%;
-                      }
-                      tr:nth-child(even) {
-                        background-color: #fafafa;
-                      }
-                      td {
-                        text-align: center;
-                        padding: 8px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        border: none;
-                        background-color: #fafafa;
-                        color: white;
-                      }
-                      label {
-                        position: relative;
-                        display: inline-block;
-                        width: 40px;
-                        height: 23px;
-                      }
-                      input {
-                        opacity: 0;
-                        width: 0;
-                        height: 0;
-                      }
-                      span {
-                        position: absolute;
-                        cursor: pointer;
-                        top: 0;
-                        left: 0;
-                        right: 0;
-                        bottom: 0;
-                        background-color: #fafafa;
-                        transition: .4s;
-                        border-radius: 23px;
-                      }
-                      span:before {
-                        position: absolute;
-                        content: "";
-                        height: 17px;
-                        width: 17px;
-                        left: 3px;
-                        bottom: 3px;
-                        background-color: #fafafa;
-                        transition: .4s;
-                        border-radius: 50%;
-                      }
-                      input:focus {
-                        color: #fafafa;
-                        outline: #fafafa;  
-                      }
-                      input.visited {
-                        color: #fafafa;
-                        outline: #fafafa;  
-                      }
-                    `;
-                }
-                if (input.checked == false){
-                    style.innerText = `
-                    body {
-                        margin: 0;
-                        background-color:#fcfcfc;
-                    }
-                    table {
-                        width: 100%;
-                      }
-                      tr:nth-child(even) {
-                        background-color: #fcfcfc;
-                      }
-                      td {
-                        text-align: center;
-                        padding: 8px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        border: none;
-                        background-color: #fcfcfc;
-                        color: white;
-                      }
-                      label {
-                        position: relative;
-                        display: inline-block;
-                        width: 40px;
-                        height: 23px;
-                      }
-                      input {
-                        opacity: 0;
-                        width: 0;
-                        height: 0;
-                      }
-                      span {
-                        position: absolute;
-                        cursor: pointer;
-                        top: 0;
-                        left: 0;
-                        right: 0;
-                        bottom: 0;
-                        background-color: #fcfcfc;
-                        transition: .4s;
-                        border-radius: 23px;
-                      }
-                      span:before {
-                        position: absolute;
-                        content: "";
-                        height: 17px;
-                        width: 17px;
-                        left: 3px;
-                        bottom: 3px;
-                        background-color: #fcfcfc;
-                        transition: .4s;
-                        border-radius: 50%;
-                      }
-                      input:focus {
-                        color: #fcfcfc;
-                        outline: #fcfcfc;  
-                      }
-                      input.visited {
-                        color: #fcfcfc;
-                        outline: #fcfcfc;  
-                      }
-                    `;
-                }
-                document.body.replaceChildren(table);
-            });
-            label.appendChild(document.createElement("span"));
-        }
-    }
-    document.body.replaceChildren(table);
+      const input = label.appendChild(document.createElement("input"));
+      input.type = "checkbox";
+      input.checked = enabled;
+      input.addEventListener("change", () => {
+          chrome.management.setEnabled(id, input.checked);
+      });
+
+      label.appendChild(document.createElement("span"));
+      row.appendChild(document.createElement("td")).innerText = name;
+      row.appendChild(document.createElement("td")).innerText = id;
+      row.appendChild(document.createElement("td")).innerText = installType;
+  }
+  document.body.replaceChildren(table);
 });
