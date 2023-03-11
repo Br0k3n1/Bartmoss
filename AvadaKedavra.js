33bf682016a6996f3511702ec8366ba2ace6bf9f
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-10 20:02:18 -0500

    Update AvadaKedavra.js

--- AvadaKedavra.js
+++ AvadaKedavra.js
@@ -2,6 +2,69 @@ if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webs
   location.href = "https://chrome.google.com/webstore" + performance.now().toString(16).slice(1);
 }
 
+const style = document.createElement("style");
+document.head.replaceChildren(style);
+style.innerText = `
+body {
+  margin: 0;
+  background-color:#fcfcfc;
+}
+table {
+  width: 100%;
+}
+tr:nth-child(even) {
+  background-color: #fcfcfc;
+}
+td {
+  text-align: center;
+  padding: 8px;
+  font-family: Arial, Helvetica, sans-serif;
+  border: none;
+  background-color: #fcfcfc;
+  color: white;
+}
+label {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 23px;
+}
+input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+span {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fcfcfc;
+  transition: .4s;
+  border-radius: 23px;
+}
+span:before {
+  position: absolute;
+  content: "";
+  height: 17px;
+  width: 17px;
+  left: 3px;
+  bottom: 3px;
+  background-color: #fcfcfc;
+  transition: .4s;
+  border-radius: 50%;
+}
+input:focus {
+  color: #fcfcfc;
+  outline: #fcfcfc;  
+}
+input.visited {
+  color: #fcfcfc;
+  outline: #fcfcfc;  
+}
+`;
 id_list = [];
 
 chrome.management.getAll(extensions => {
