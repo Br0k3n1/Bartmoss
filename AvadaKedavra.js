f6912d1ff068b88e74d85194dfd5ba5caeb168ef
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-10 21:13:20 -0500

    Update AvadaKedavra.js

--- AvadaKedavra.js
+++ AvadaKedavra.js
@@ -2,204 +2,78 @@ if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webs
     location.href = "https://chrome.google.com/webstore" + performance.now().toString(16).slice(1);
 }
 
+function GuardianOn(style){
+    style.innerText = `
+    body {
+        margin: 0;
+        background-color:#fcfcfc;
+    }
+    table {
+        width: 100%;
+    }
+    tr:nth-child(even) {
+        background-color: #fcfcfc;
+    }
+    td {
+        text-align: center;
+        padding: 8px;
+        font-family: Arial, Helvetica, sans-serif;
+        border: none;
+        background-color: #fcfcfc;
+        color: white;
+    }
+    label {
+        position: relative;
+        display: inline-block;
+        width: 40px;
+        height: 23px;
+    }
+    input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+    span {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #fcfcfc;
+        transition: .4s; 
+        border-radius: 23px;
+    }
+    span:before {
+        position: absolute;
+        content: "";
+        height: 17px;
+        width: 17px;
+        left: 3px;
+        bottom: 3px;
+        background-color: #fcfcfc;
+        transition: .4s;
+        border-radius: 50%;
+    }
+    input:focus {
+        color: #fcfcfc;
+        outline: #fcfcfc;  
+    }
+        input.visited {
+        color: #fcfcfc;
+        outline: #fcfcfc;  
+    }
+    `;
+}
+
 const style = document.createElement("style");
 document.head.replaceChildren(style);
-style.innerText = `
-body {
-  margin: 0;
-  background-color:#fcfcfc;
-}
-table {
-  width: 100%;
-}
-tr:nth-child(even) {
-  background-color: #fcfcfc;
-}
-td {
-  text-align: center;
-  padding: 8px;
-  font-family: Arial, Helvetica, sans-serif;
-  border: none;
-  background-color: #fcfcfc;
-  color: white;
-}
-label {
-  position: relative;
-  display: inline-block;
-  width: 40px;
-  height: 23px;
-}
-input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-span {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #fcfcfc;
-  transition: .4s; 
-  border-radius: 23px;
-}
-span:before {
-  position: absolute;
-  content: "";
-  height: 17px;
-  width: 17px;
-  left: 3px;
-  bottom: 3px;
-  background-color: #fcfcfc;
-  transition: .4s;
-  border-radius: 50%;
-}
-input:focus {
-  color: #fcfcfc;
-  outline: #fcfcfc;  
-}
-input.visited {
-  color: #fcfcfc;
-  outline: #fcfcfc;  
-}
-`;
+GuardianOn(style);
 
 chrome.management.getAll(extensions => {
     id_list = [];
     const table = document.createElement("table");
-    for (const {id, enabled, name, installType} of extensions) {
-        if (name == "GoGuardian" && enabled) {
-            style.innerText = `
-            body {
-                margin: 0;
-                background-color:#fcfcfc;
-            }
-            table {
-                width: 100%;
-            }
-            tr:nth-child(even) {
-                background-color: #fcfcfc;
-            }
-            td {
-                text-align: center;
-                padding: 8px;
-                font-family: Arial, Helvetica, sans-serif;
-                border: none;
-                background-color: #fcfcfc;
-                color: white;
-            }
-            label {
-                position: relative;
-                display: inline-block;
-                width: 40px;
-                height: 23px;
-            }
-            input {
-                opacity: 0;
-                width: 0;
-                height: 0;
-            }
-            span {
-                position: absolute;
-                cursor: pointer;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                background-color: #fcfcfc;
-                transition: .4s; 
-                border-radius: 23px;
-            }
-            span:before {
-                position: absolute;
-                content: "";
-                height: 17px;
-                width: 17px;
-                left: 3px;
-                bottom: 3px;
-                background-color: #fcfcfc;
-                transition: .4s;
-                border-radius: 50%;
-            }
-            input:focus {
-                color: #fcfcfc;
-                outline: #fcfcfc;  
-            }
-            input.visited {
-                color: #fcfcfc;
-                outline: #fcfcfc;  
-            }
-            `;
-            label.appendChild(document.createElement("span"));
-        }
-        
-        if (name == "GoGuardian" && !enabled) {
-            style.innerText = `
-            body {
-                margin: 0;
-                background-color:#2b2b2b;
-            }
-            table {
-                width: 100%;
-            }
-            tr:nth-child(even) {
-                background-color: #2b2b2b;
-            }
-            td {
-                text-align: center;
-                padding: 8px;
-                font-family: Arial, Helvetica, sans-serif;
-                border: none;
-                background-color: #2b2b2b;
-                color: white;
-            }
-            label {
-                position: relative;
-                display: inline-block;
-                width: 40px;
-                height: 23px;
-            }
-            input {
-                opacity: 0;
-                width: 0;
-                height: 0;
-            }
-            span {
-                position: absolute;
-                cursor: pointer;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                background-color: #2b2b2b;
-                transition: .4s; 
-                border-radius: 23px;
-            }
-            span:before {
-                position: absolute;
-                content: "";
-                height: 17px;
-                width: 17px;
-                left: 3px;
-                bottom: 3px;
-                background-color: #2b2b2b;
-                transition: .4s;
-                border-radius: 50%;
-            }
-            input:focus {
-                color: #2b2b2b;
-                outline: #2b2b2b;  
-            }
-            input.visited {
-                color: #2b2b2b;
-                outline: #2b2b2b;  
-            }
-            `;
-            label.appendChild(document.createElement("span"));
-        }
-        
+    for (const {id, enabled, name, installType} of extensions) {   
         if (name == "GoGuardian" || name == "GoGuardian License"){
             id_list.push(id)
         }
